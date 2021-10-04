41b610a79bd2a0e6188805e6ab8e135dac15fe55
VB2007 <78029857+VB2007@users.noreply.github.com>
2021-10-04 17:01:08 +0700

    First string update

--- main.py
+++ main.py
@@ -8,9 +8,10 @@ today_date = "{:02d}".format(datetime.date.today().day)
 today_date += "-" + "{:02d}".format(datetime.date.today().month)
 today_date += "-" + str(datetime.date.today().year)
 
-names = all_names()
+names = all_names()[1:]
 
 for name in names:
+    name[3]=name[3].split('.')[0]
     UID = uuid.uuid4().hex
     file = PPTX_GENERATOR(name, UID, today_date)
     command = "python PPTX_to_PDF.py " + file + " " + today_date
