b13a288740fe1c666c7c2ef96762e49b13f7eb4e
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-26 22:32:20 +0700

    Checking gender firstly by last letter of middle name and if it is no middle name checking it in old way

--- morphy_using.py
+++ morphy_using.py
@@ -3,23 +3,37 @@ from pytrovich.enums import NamePart, Gender, Case
 from pytrovich.maker import PetrovichDeclinationMaker
 
 
-def name_change(name):
-
+def name_change(name, case='dative'):
+    # Нужно для свободного выбора падежа
+    all_cases = {
+    'genetive': Case.GENITIVE, 
+    'accusative': Case.ACCUSATIVE, 
+    'dative': Case.DATIVE, 
+    'instrumental': Case.INSTRUMENTAL, 
+    'prepositional': Case.PREPOSITIONAL
+    }
     morph = pmr.MorphAnalyzer(lang='ru')  # определение пола
     maker = PetrovichDeclinationMaker()  # склонение ФИО
     full_name = name.split()  # разбиение ФИО на составляющие
 
     gender = ""
-    if full_name[1]:
+    # Определяем пол по отчеству человека (если оно имеется)
+    # Если последняя буква - ч, то это мужчина, иначе женщина
+    if full_name[-1]:
+        if full_name[-1][-1].lower() == 'ч':
+            gender = Gender.MALE
+        else:
+            gender = Gender.FEMALE
+    else:
         if 'masc' in morph.parse(full_name[1])[0].tag:
             gender = Gender.MALE
         elif 'femn' in morph.parse(full_name[1])[0].tag:
             gender = Gender.FEMALE
 
     cased_first_name = maker.make(
-        NamePart.FIRSTNAME, gender, Case.DATIVE, full_name[1])
+        NamePart.FIRSTNAME, gender, all_cases[case], full_name[1])
     cased_sur_name = maker.make(
-        NamePart.LASTNAME, gender, Case.DATIVE, full_name[0])
+        NamePart.LASTNAME, gender, all_cases[case], full_name[0])
     #  cased_patronymic = maker.make(
     #     NamePart.MIDDLENAME, gender, Case.DATIVE, full_name[2])
 
