05baa5398250568eee8a005a3bb03f4db79f5425
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-26 22:35:38 +0700

    Updated casing thing

--- all_names.py
+++ all_names.py
@@ -4,24 +4,32 @@ import uuid
 
 
 def all_names(work_list, template):
-    print(work_list, template)
     if work_list == '':
         work_list = 'list.xlsx'
     else:
         # Меняем расширение файла на .xlsx на случай если человек выберет немного другой формат Excel таблицы
-        work_list = work_list.split('.')[0]+'.xlsx'
+        parts = work_list.split('.')
+        if len(parts) >= 3:
+            work_list  = ''
+            for i in range(len(parts) - 1):
+                work_list += parts[i] + "."
+            work_list += 'xlsx'
     # Для работы с Excel таблицей
     wb = tuple(load_workbook(work_list).active.values)
     ans = list()
     for i in range(1, len(wb)):
         a = dict()
         for j in range(len(wb[i])):
-            a[wb[0][j]] = wb[i][j]
+            # Это сделано чтобы указывать падеж только на одной строке, а не на каждой
+            if wb[0][j] == 'case':
+                a[wb[0][j]] = wb[1][j]
+            else:
+                a[wb[0][j]] = wb[i][j]
         a['template'] = template
         a['date'] = "{:02d}".format(datetime.date.today().day) + "." + "{:02d}".format(
             datetime.date.today().month) + "." + str(datetime.date.today().year)
         a['id'] = uuid.uuid4().hex
-        a['file_name'] = wb[i][0]
+        a['file_name'] = a['name']
 
         ans.append(a)
     return ans
