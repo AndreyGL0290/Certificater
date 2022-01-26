521319a2c583ab5f0eb0eb472a28c2d504ae4f10
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-26 22:30:23 +0700

    Made all required changes except pptx to pdf fast convert

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ path = getcwd()
 
 eel.init(path + "\\Web")
 
+'''
+Сделано:
+Русские символы в названии файлов
+Возможность не склонять ФИО
+Модификация склонения (по отчеству, если его нет, то morphy)
+НЕ СКЛОНЯТЬ другие текстовые поля
+При наличии не используемых полей возникает ошибка
+
+Надо сделать:
+Не открывать и закрывать каждый раз Power Point
+'''
+
+
 @eel.expose
 def start(input_file_name, output_file_name):
     from PPTX_to_PDF import main
@@ -12,10 +25,9 @@ def start(input_file_name, output_file_name):
     from PPTX_GENERATOR import PPTX_GENERATOR
     import os
     import shutil
-    print(input_file_name, output_file_name)
     data = all_names(input_file_name, output_file_name)
-    # То что ниже нужно для тетинга
-    # data = all_names('list1.xlsx', 'tmplt1.pptx')
+    # То что ниже нужно для тестинга
+    # data = all_names('data.xlsx', 'tmplt1.pptx')
 
     os.makedirs(f"GENERATED_PPTX/{data[0]['date']}",
                 exist_ok=True)
@@ -31,9 +43,9 @@ def start(input_file_name, output_file_name):
                 exist_ok=True)
     for loc in data:
         file_name = PPTX_GENERATOR(loc)
-        command = "python PPTX_to_PDF.py " + file_name + " " + loc['date']
+        # command = "python PPTX_to_PDF.py " + file_name + " " + loc['date']
         main(file_name, loc['date'])
-        file = file_name.replace("©", " ")
+        # file = file_name.replace("©", " ")
 
 if __name__ == "__main__":
     eel.start("HomePage.html", geometry={"size": (600, 400), "position": (400, 600)}, port=8002)
