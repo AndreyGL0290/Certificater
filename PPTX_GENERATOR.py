282e1f86594bfb2ed1f15f8a3a2fd064a4f4c2fc
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-26 22:33:10 +0700

    Updated full name casing

--- PPTX_GENERATOR.py
+++ PPTX_GENERATOR.py
@@ -1,20 +1,24 @@
 from morphy_using import name_change
 from pptx import Presentation
-import os
-
 
 def PPTX_GENERATOR(data):
     prs = Presentation(data['template'])
     for shape in prs.slides[0].shapes:  # перебираем объекты на слайде
         if (shape.has_text_frame):
-            for i in data.items():
+            for key, value in data.items():
+                
                 try:
-                    if i[0] == shape.text_frame.text.lower():
-                        shape.text_frame.paragraphs[0].runs[0].text = name_change(
-                            i[1])
+                    if key == shape.text_frame.text.lower() == 'name':
+                        # Меняем слово-заместитель в презентации на склоненное значение этого слова в нашем словаре data
+                        try:
+                            shape.text_frame.paragraphs[0].runs[0].text = name_change(value, data['case'])
+                        # Если падеж не указан отдельно в Excel файле, то используем значение по умолчанию
+                        except KeyError:
+                            shape.text_frame.paragraphs[0].runs[0].text = name_change(value)
                 except:
-                    if i[0] == shape.text_frame.text.lower():
-                        shape.text_frame.paragraphs[0].runs[0].text = i[1]
+                    if key == shape.text_frame.text.lower():
+                        # Меняем слово-заместитель в презентации на значение этого слова в нашем словаре data
+                        shape.text_frame.paragraphs[0].runs[0].text = value
 
     # указываем создателя
     prs.core_properties.author = ""
@@ -23,8 +27,7 @@ def PPTX_GENERATOR(data):
 
     #  для хранения сертификатов создаём две папки, в которых тоже будут папки
     #  внитри GENERATED_PPTX и GENERATED_PDF будут папки-даты
-    
-    prs.save('GENERATED_PPTX/' + data['date'] + '/' +
-             data['file_name'] + '_' + data['id'] + '.pptx')
+    prs.save('GENERATED_PPTX/' + data['date'] + '/' + data['file_name'] + '_' + data['id'] + '.pptx')
     # имя файла для перевода его в PDF
-    return(data['file_name'] + '_' + data['id'])
+    return (data['file_name'] + '_' + data['id'])
+3
