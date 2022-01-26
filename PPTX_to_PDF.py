1ccd7a628c1108c90ed2bdfdbfd9ee7bfb93751b
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-26 22:34:08 +0700

    Made small refactoring and nothing else

--- PPTX_to_PDF.py
+++ PPTX_to_PDF.py
@@ -4,7 +4,7 @@ import os
 def init_powerpoint():
     powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
     powerpoint.UserControl = 0
-    powerpoint.Visible = 1
+    
     return powerpoint
  
 def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType = 32):
@@ -22,4 +22,4 @@ def main(file, today_date):
     powerpoint = init_powerpoint()  # инициализируем процесс PowerPoint (работает ТОЛЬКО в Windows)
     cwd = os.getcwd() + "\GENERATED_PPTX\\"+ today_date +"\\"+ file + ".pptx"  # создаём полный путь до файла 
     ppt_to_pdf(powerpoint, cwd, cwd)  # запуск конвертации 
-    powerpoint.Quit()  
+    powerpoint.Quit()
