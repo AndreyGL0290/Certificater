888be53c4b29059ca4dfffe1938b8bb67221ef23
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-31 18:09:56 +0700

    PEP8 refactoring

--- morphy_using.py
+++ morphy_using.py
@@ -3,7 +3,7 @@ from pytrovich.enums import NamePart, Gender, Case
 from pytrovich.maker import PetrovichDeclinationMaker
 
 
-def name_change(name, case='dative'):
+def name_change(name='Иванов Иван Иванович', case='dative'):
     # Нужно для свободного выбора падежа
     all_cases = {
     'genetive': Case.GENITIVE, 
@@ -41,43 +41,3 @@ def name_change(name, case='dative'):
     final_name = final_name.strip()
 
     return final_name
-
-
-'''
-    f_name = ''
-    print()
-    print(morph.parse(name))
-    for elem in morph.parse(name):
-        print(elem)
-        if 'Patr' in elem.tag:
-            f_name = elem
-    print()
-
-    is_name = any('Patr' in p.tag for p in morph.parse(name))
-    print(is_name)
-    print("Ok")
-'''
-
-'''
-    if 'masc' in morph.parse(full_name[1])[0].tag:
-        gender = Gender.MALE
-    else:
-        gender = Gender.FEMALE
-    
-    if 'masc' in morph.parse(full_name[1])[0].tag:
-        cased_last_name = maker.make(NamePart.LASTNAME, Gender.MALE, Case.DATIVE, full_name[0])
-        cased_name = maker.make(NamePart.FIRSTNAME, Gender.MALE, Case.DATIVE, full_name[1])
-    elif 'femn' in morph.parse(full_name[1])[0].tag:
-        cased_last_name = maker.make(NamePart.LASTNAME, Gender.FEMALE, Case.DATIVE, full_name[0])
-        cased_name = maker.make(NamePart.FIRSTNAME, Gender.FEMALE, Case.DATIVE, full_name[1])
-    try:
-        if 'masc' in morph.parse(full_name[1])[0].tag:
-            cased_middle_name = maker.make(NamePart.MIDDLENAME, Gender.MALE, Case.DATIVE, full_name[2])
-            print("Hi", cased_middle_name)
-        elif 'femn' in morph.parse(full_name[1])[0].tag:
-            cased_middle_name = maker.make(NamePart.MIDDLENAME, Gender.FEMALE, Case.DATIVE, full_name[2])
-
-        return [cased_last_name, cased_name, cased_middle_name]
-    except IndexError:
-        return [cased_last_name, cased_name, '']
-'''
