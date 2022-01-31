d5644819117b647980e8a897c31941f39623490b
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-31 18:09:10 +0700

    PEP8 refactoring

--- PPTX_to_PDF.py
+++ PPTX_to_PDF.py
@@ -1,11 +1,4 @@
-import comtypes.client
 import os
-
-def init_powerpoint():
-    powerpoint = comtypes.client.CreateObject("Powerpoint.Application")
-    powerpoint.UserControl = 0
-    
-    return powerpoint
  
 def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType = 32):
     if outputFileName[-3:] != 'pdf':
@@ -15,11 +8,11 @@ def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType = 32):
     deck.Close()
     #print('convert %s file complete '%outputFileName)
 
-def main(file, today_date):
+def main(file, today_date, powerpoint):
     # file = sys.argv[1]  # из аргументов командной строки получаем имя файла
     # today_date = sys.argv[2]  # и дату
     file = file.replace("©", " ")  # возвращаем обратно пробелы 
-    powerpoint = init_powerpoint()  # инициализируем процесс PowerPoint (работает ТОЛЬКО в Windows)
-    cwd = os.getcwd() + "\GENERATED_PPTX\\"+ today_date +"\\"+ file + ".pptx"  # создаём полный путь до файла 
+    # powerpoint = init_powerpoint()  # инициализируем процесс PowerPoint (работает ТОЛЬКО в Windows)
+    cwd = f"{os.getcwd()}\\GENERATED_PPTX\\{today_date}\\{file}.pptx"  # создаём полный путь до файла 
     ppt_to_pdf(powerpoint, cwd, cwd)  # запуск конвертации 
-    powerpoint.Quit()
+    # powerpoint.Quit()
