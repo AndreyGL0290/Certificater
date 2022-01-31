bb860ffe2f920bd95a4053c9418d876f03492ecc
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-01-31 18:07:31 +0700

    Added checbox for email sending

--- Web/script.js
+++ Web/script.js
@@ -2,6 +2,7 @@ const create = document.getElementById("create");
 
 let input = document.getElementById("input-file");
 let output = document.getElementById("output-file");
+let checkbox = document.getElementById("send");
 
 input.addEventListener('change', () => {
     document.getElementsByClassName("choosed-file-label")[0].textContent = input.files[0].name;
@@ -11,6 +12,13 @@ output.addEventListener('change', () => {
     document.getElementsByClassName("choosed-file-label")[1].textContent = output.files[0].name
 });
 
+// Когда нажимается кнопка "Отправить после сохранения" ее значение меняется
+let value = false;
+checkbox.addEventListener('click', () => {
+    if (!value) value = true;
+    else value = false;
+});
+
 create.addEventListener("click", () => {
-    eel.start(input.files[0].name, output.files[0].name);
+    eel.start(input.files[0].name, output.files[0].name, value);
 });
