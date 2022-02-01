1257afb8c87c78c31356123cf9f72e808ff3b35f
Andrew Glukhov <andriy.glukhov@yandex.rucd>
2022-02-01 12:27:49 +0700

    Added popup window with email form

--- Web/script.js
+++ Web/script.js
@@ -4,6 +4,32 @@ let input = document.getElementById("input-file");
 let output = document.getElementById("output-file");
 let checkbox = document.getElementById("send");
 
+// Pop Up
+let popupBg = document.getElementsByClassName('popup__bg')[0]; // Фон попап окна
+let popup = document.getElementsByClassName('popup')[0]; // Само окно
+let openPopupButton = document.getElementsByClassName('open-popup')[0]; // Кнопки для показа окна
+let closePopupButton = document.getElementsByClassName('close-popup')[0]; // Кнопка для скрытия окна
+
+openPopupButton.addEventListener('click', (e) => { // Для каждой вешаем обработчик событий на клик
+    e.preventDefault(); // Предотвращаем дефолтное поведение браузера
+    popupBg.classList.add('active'); // Добавляем класс 'active' для фона
+    popup.classList.add('active'); // И для самого окна
+})
+
+closePopupButton.addEventListener('click', () => { // Вешаем обработчик на крестик
+    popupBg.classList.remove('active'); // Убираем активный класс с фона
+    popup.classList.remove('active'); // И с окна
+});
+
+document.addEventListener('click', (e) => { // Вешаем обработчик на весь документ
+    if (e.target === popupBg) { // Если цель клика - фон, то:
+        popupBg.classList.remove('active'); // Убираем активный класс с фона
+        popup.classList.remove('active'); // И с окна
+    }
+});
+
+let emailForm = document.forms[popup.getAttribute('name')];
+
 input.addEventListener('change', () => {
     document.getElementsByClassName("choosed-file-label")[0].textContent = input.files[0].name;
 });
@@ -20,5 +46,23 @@ checkbox.addEventListener('click', () => {
 });
 
 create.addEventListener("click", () => {
-    eel.start(input.files[0].name, output.files[0].name, value);
+    try {
+        eel.start(input.files[0].name, output.files[0].name, value);
+    } catch (error) {
+        if (error instanceof TypeError) document.getElementById('error').textContent = "Выберите файлы";
+        else document.getElementById('error').textContent = "Что то пошло не так...";
+    }
+
 });
+
+document.getElementById('send-email').addEventListener('click', () => {
+    let email = emailForm.elements["email"].value;
+    let password = emailForm.elements["password"].value;
+    console.log(email, password);
+    eel.create_email_info(email, password) // Оба из формы
+})
+
+eel.expose(raise_error);
+function raise_error(error, place_id) {
+    document.getElementById(place_id).textContent = error;
+}
